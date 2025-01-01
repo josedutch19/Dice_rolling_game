cd08496859607dcf7821a6691857cd38c807dc3c
josedutch <josedutch19@gmail.com>
2025-01-01 10:04:52 -0600

    Initial commit

---
+++ dice_rolling_game.py
@@ -0,0 +1,10 @@
+# Loop
+choice = input('Roll the dice? (y/n):')# Ask: roll the device?
+# If user enters y
+#   Generate two random umbers
+#   Print them
+# If user enters n
+#   Print thank you message 
+#   Terminate 
+# Else
+#   Print invalid choice
