4d25c84f2fd7e93d19d60f7d91638c3b7e271201
josedutch <josedutch19@gmail.com>
2025-01-01 10:13:50 -0600

    Added Choice

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -1,6 +1,6 @@
 # Loop
 choice = input('Roll the dice? (y/n):')# Ask: roll the device?
-# If user enters y
+if choice == 'y'# If user enters y
 #   Generate two random umbers
 #   Print them
 # If user enters n
